cbf9eaa73df4fc3599b8b5b518ab71e60e41c591
Mircea Toda <mitoda@cisco.com>
2017-04-25 06:32:25 -0400

    committed for run

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+with open('README.txt') as file:
+    long_description = file.read()
+
+setup(
+    # MANDATORY fields
+    # Metadata for upload to kick-pypi
+    packages = find_packages(), # do not remove this line. used for adding files into package
+    name = "packageName", # same as template name
+    author = "Dummy User",
+    author_email = "<CEC_userid>@cisco.com", # if author is supplied author_email is mandatory too
+    url = "http://my-home-page.example.com",
+
+    # OPTIONAL fields (see https://docs.python.org/2/distutils/setupscript.html for more)
+    long_description = long_description,
+    # version = "1.0.0" # if not supplied default will be 0.0.0
+    # description = "This is an Example Package"
+    # install_requires = ['docutils>=0.3'] # for setting package dependencies
+    # license = "Licensed to Dummy"
+    # keywords = "hello world example examples"
+)
